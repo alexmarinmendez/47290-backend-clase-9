60c6dcf14d6ffd9115f39d4d30d9d13fd9f51ebd
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-08-16 19:32:52 -0500

    refactor: create users.router

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import handlebars from 'express-handlebars'
+import usersRouter from './routers/users.router.js'
 
 const app = express()
 
@@ -8,12 +9,5 @@ app.engine('handlebars', handlebars.engine())
 app.set('views', './src/views')
 app.set('view engine', 'handlebars')
 
-app.get('/', (req, res) => {
-    //TODO: se obtiene el dato (nombre) del user
-    res.render('home', {
-        name: 'Emiliano',
-        lastname: 'Grange'
-    })
-})
-
+app.use('/users', usersRouter)
 app.listen(8080, () => console.log('Server Up!'))
